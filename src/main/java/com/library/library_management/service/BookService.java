807ccbc40a967354a572dd7f4d424df92e9eab6c
Bogdan <wwwterleckiy@gmail.com>
2025-06-04 21:43:50 +0300

    feat: add BookService

--- src/main/java/com/library/library_management/service/BookService.java
+++ src/main/java/com/library/library_management/service/BookService.java
@@ -1,4 +1,19 @@
 package com.library.library_management.service;
 
+import com.library.library_management.dto.BookResponse;
+import com.library.library_management.dto.CreateBookRequest;
+import com.library.library_management.dto.UpdateBookRequest;
+
+import java.util.List;
+
 public interface BookService {
+    BookResponse createBook(CreateBookRequest request);
+
+    BookResponse getBookById(Long id);
+
+    List<BookResponse> getAllBooks();
+
+    BookResponse updateBookById(Long id, UpdateBookRequest request);
+
+    void deleteBookById(Long id);
 }
