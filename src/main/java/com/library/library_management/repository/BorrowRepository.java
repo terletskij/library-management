09913256d796e85911eda01ec85a0007b2a7af49
Bogdan <wwwterleckiy@gmail.com>
2025-06-05 12:53:35 +0300

    feat(borrow): add custom method for borrow checks

--- src/main/java/com/library/library_management/repository/BorrowRepository.java
+++ src/main/java/com/library/library_management/repository/BorrowRepository.java
@@ -3,6 +3,8 @@ package com.library.library_management.repository;
 import com.library.library_management.entity.Borrow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BorrowRepository extends JpaRepository<Borrow, Long> {
 
     boolean existsByBookIdAndReturnDateIsNull(Long bookId);
@@ -10,4 +12,6 @@ public interface BorrowRepository extends JpaRepository<Borrow, Long> {
     boolean existsByMemberIdAndReturnDateIsNull(Long memberId);
 
     int countByMemberIdAndReturnDateIsNull(Long memberId);
+
+    List<Borrow> findByMemberIdAndReturnDateIsNull(Long memberId);
 }
