7d7df08c27f3c796e092aaf2215dc757015d3a08
Bogdan <wwwterleckiy@gmail.com>
2025-06-05 12:54:36 +0300

    feat(member): support borrow logic

--- src/main/java/com/library/library_management/repository/MemberRepository.java
+++ src/main/java/com/library/library_management/repository/MemberRepository.java
@@ -1,4 +1,7 @@
 package com.library.library_management.repository;
 
-public interface MemberRepository {
+import com.library.library_management.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
