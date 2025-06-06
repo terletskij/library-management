159afac3936d6fc8fb7e06732ffdf8974fab1cdf
Bogdan <wwwterleckiy@gmail.com>
2025-06-07 01:55:35 +0300

    feat(MemberRepository): add findByName method

--- src/main/java/com/library/library_management/repository/MemberRepository.java
+++ src/main/java/com/library/library_management/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.library.library_management.repository;
 import com.library.library_management.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);
 }
